38aae8dad3110cc7e94f83332574d150251f459e
Andrey Kuzmin <prepodam@mail.ru>
2022-07-25 00:23:48 +0300

    Tidy.

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-import * as vscode from 'vscode';
-import { spawn } from 'child_process';
-import { dirname, join, isAbsolute } from 'path';
-import { existsSync } from 'fs';
-import { FormatError, handleTidyError, isErrnoException } from './error';
+import * as vscode from 'vscode'
+import { spawn } from 'child_process'
+import { dirname, join, isAbsolute } from 'path'
+import { existsSync } from 'fs'
+import { FormatError, handleTidyError, isErrnoException } from './error'
 
 export function activate(context: vscode.ExtensionContext) {
-  const selector = ['perl', 'perl+mojolicious'];
+  const selector = ['perl', 'perl+mojolicious']
   let perltidyVersion: Number
 
   function getExecutable() {
@@ -33,7 +33,8 @@ export function activate(context: vscode.ExtensionContext) {
 
         worker.stdout.on('data', (chunk) => {
           resultText += chunk
-        });
+        })
+
         worker.stdout.on('end', () => {
           const res = resultText.match(/\d+/)
           if (res === null) {
@@ -55,16 +56,16 @@ export function activate(context: vscode.ExtensionContext) {
 
   function get_range(document: vscode.TextDocument, range: vscode.Range | null, selection: vscode.Selection | null) {
     if (!(selection === null) && !selection.isEmpty) {
-      range = new vscode.Range(selection.start, selection.end);
+      range = new vscode.Range(selection.start, selection.end)
     }
 
     if (range === null) {
-      let start = new vscode.Position(0, 0);
-      let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-      range = new vscode.Range(start, end);
+      let start = new vscode.Position(0, 0)
+      let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length)
+      range = new vscode.Range(start, end)
     }
 
-    return range;
+    return range
   }
 
   /**
@@ -76,34 +77,34 @@ export function activate(context: vscode.ExtensionContext) {
    * @throws {unknown} Throw an error an unexpected problem has occurred.
    */
   function tidy(document: vscode.TextDocument, range: vscode.Range): Promise<string | undefined> {
-    let text = document.getText(range);
-    if (!text || text.length === 0) return new Promise((resolve) => { resolve('') });
+    let text = document.getText(range)
+    if (!text || text.length === 0) return new Promise((resolve) => { resolve('') })
 
-    let config = vscode.workspace.getConfiguration('perltidy-more');
+    let config = vscode.workspace.getConfiguration('perltidy-more')
 
-    var executable = getExecutable();
-    let profile = config.get('profile', '');
+    var executable = getExecutable()
+    let profile = config.get('profile', '')
 
     const currentWorkspace = vscode.workspace.getWorkspaceFolder(
       document.uri
     )
 
     if (currentWorkspace === undefined) {
-      throw new FormatError('Format failed. File must be belong to one workspace at least.');
+      throw new FormatError('Format failed. File must be belong to one workspace at least.')
     }
 
     if (config.get('autoDisable', false)) {
       if (!existsSync(join(currentWorkspace.uri.path, '.perltidyrc'))) {
-        return Promise.resolve(undefined);
+        return Promise.resolve(undefined)
       }
     }
 
     let args: string[] = [
       "--standard-output"
-    ];
+    ]
 
     if (profile) {
-      args.push("--profile=" + profile);
+      args.push("--profile=" + profile)
     }
 
     // Only supported starts with this version
@@ -113,16 +114,16 @@ export function activate(context: vscode.ExtensionContext) {
       // With terminal newline: "foo\n" -> "foo\n", "foo" -> "foo\n"
       // Expected result:       "foo\n" -> "foo\n", "foo" -> "foo"
 
-      args.push("--no-add-terminal-newline");
+      args.push("--no-add-terminal-newline")
     }
 
     let options = {
       cwd: dirname(document.uri.fsPath)
-    };
+    }
 
     // Support for spawn at virtual filesystems
     if (document.uri.scheme != "file") {
-      options.cwd = ".";
+      options.cwd = "."
     }
 
     // Support for executing relative path script from the current workspace. eg: ./script/perltidy-wrapper.pl
@@ -130,64 +131,64 @@ export function activate(context: vscode.ExtensionContext) {
       let resolved = join(currentWorkspace.uri.path, executable)
 
       if (existsSync(resolved)) {
-        executable = resolved;
+        executable = resolved
 
         // Also we change cwd to support for local .perltidyrc in case of run it
         // in docker image (may be cwd will be set to workspace folder for all
         // cases)
-        options.cwd = currentWorkspace.uri.path;
+        options.cwd = currentWorkspace.uri.path
       }
     }
 
     return new Promise((resolve, reject) => {
       try {
-        let worker = spawn(executable, args, options);
+        let worker = spawn(executable, args, options)
 
-        worker.stdin.write(text);
-        worker.stdin.end();
+        worker.stdin.write(text)
+        worker.stdin.end()
 
-        let result_text = '';
-        let error_text = '';
+        let result_text = ''
+        let error_text = ''
 
         worker.on('error', (e) => {
           // When the process fails to start, terminate, or send a message to the process
           // ref: https://nodejs.org/api/child_process.html#child_process_event_error
           if (isErrnoException(e) && e.code === 'ENOENT') {
             if (executable === 'perltidy') {
-              reject(new FormatError(`Format failed. Executable file (\`${executable}\`) is not found. You probably forgot to install perltidy.`));
+              reject(new FormatError(`Format failed. Executable file (\`${executable}\`) is not found. You probably forgot to install perltidy.`))
             } else {
-              reject(new FormatError(`Format failed. Executable file (\`${executable}\`) is not found.`));
+              reject(new FormatError(`Format failed. Executable file (\`${executable}\`) is not found.`))
             }
           } else {
-            reject(e);
+            reject(e)
           }
-        });
+        })
 
         worker.stdout.on('data', (chunk) => {
-          result_text += chunk;
-        });
+          result_text += chunk
+        })
         worker.stderr.on('data', (chunk) => {
-          error_text += chunk;
-        });
+          error_text += chunk
+        })
 
         worker.on('close', (code) => {
           if (code !== 0) {
             // ref: http://perltidy.sourceforge.net/perltidy.html#ERROR-HANDLING
             if (error_text === '') {
-              reject(new FormatError(`Format failed. Perltidy exited with exit code ${code}.`));
+              reject(new FormatError(`Format failed. Perltidy exited with exit code ${code}.`))
             } else {
-              reject(new FormatError(`Format failed. Perltidy exited with exit code ${code}. Error messages: ${error_text}`));
+              reject(new FormatError(`Format failed. Perltidy exited with exit code ${code}. Error messages: ${error_text}`))
             }
           } else {
-            resolve(result_text);
+            resolve(result_text)
           }
-        });
+        })
       }
       catch (error) {
         // internal error
-        reject(error);
+        reject(error)
       }
-    });
+    })
   }
 
   let provider = vscode.languages.registerDocumentRangeFormattingEditProvider(selector, {
@@ -197,86 +198,96 @@ export function activate(context: vscode.ExtensionContext) {
       //
       // Don't expand if there is a non-whitespace character between the beginning of the line and the range
       // "return [do {]" -> "return [do {]"
-      const indentRange = new vscode.Range(new vscode.Position(range.start.line, 0), range.start);
+      const indentRange = new vscode.Range(new vscode.Position(range.start.line, 0), range.start)
       if (document.getText(indentRange).match(/^\s*$/)) {
-        range = new vscode.Range(new vscode.Position(range.start.line, 0), range.end);
+        range = new vscode.Range(new vscode.Position(range.start.line, 0), range.end)
       }
 
-      range = get_range(document, range, null);
+      range = get_range(document, range, null)
 
       try {
-        let res = await tidy(document, range);
-        if (res === undefined) return;
-        let result: vscode.TextEdit[] = [];
-        result.push(new vscode.TextEdit(range, res));
-        return result;
+        let res = await tidy(document, range)
+        if (res === undefined) {
+          return
+        }
+
+        let result: vscode.TextEdit[] = []
+        result.push(new vscode.TextEdit(range, res))
+        return result
       } catch (e) {
-        handleTidyError(e);
-        return;
+        handleTidyError(e)
+        return
       }
     }
-  });
+  })
 
   let formatOnTypeProvider = vscode.languages.registerOnTypeFormattingEditProvider(selector, {
     provideOnTypeFormattingEdits: async (document, position, ch, options, token) => {
         // Determine start position. start format from the next line of the previous ';'.
-        let start = new vscode.Position(0, 0);
-        let lineNumber = position.line - 1;
+        let start = new vscode.Position(0, 0)
+        let lineNumber = position.line - 1
+
         while (lineNumber >= 0) {
-          const line = document.lineAt(lineNumber);
-          const indexOfSemicolon = line.text.lastIndexOf(';');
+          const line = document.lineAt(lineNumber)
+          const indexOfSemicolon = line.text.lastIndexOf(';')
+
           if (indexOfSemicolon >= 0) {
-            start = new vscode.Position(lineNumber + 1, 0);
-            break;
+            start = new vscode.Position(lineNumber + 1, 0)
+            break
           }
-          lineNumber--;
+
+          lineNumber--
         }
-        const range = new vscode.Range(start, position);
+        const range = new vscode.Range(start, position)
 
       try {
-        const res = await tidy(document, range);
+        const res = await tidy(document, range)
         if (res === undefined || token.isCancellationRequested) {
-          return;
+          return
         }
-        const result: vscode.TextEdit[] = [];
-        result.push(new vscode.TextEdit(range, res));
-        return result;
+
+        const result: vscode.TextEdit[] = []
+        result.push(new vscode.TextEdit(range, res))
+        return result
       } catch (e) {
-        handleTidyError(e);
-        return;
+        handleTidyError(e)
+        return
       }
     }
-  }, ';', '}', ')', ']');
+  }, ';', '}', ')', ']')
 
   let command = vscode.commands.registerCommand('perltidy-more.tidy', async () => {
-    const editor = vscode.window.activeTextEditor;
+    const editor = vscode.window.activeTextEditor
     if (!editor) {
-      return;
+      return
     }
 
-    let document = editor.document;
-    let selection = editor.selection;
+    let document = editor.document
+    let selection = editor.selection
 
-    let range = get_range(document, null, selection);
+    let range = get_range(document, null, selection)
 
     try {
-      const res = await tidy(document, range);
-      if (res === undefined) return;
+      const res = await tidy(document, range)
+      if (res === undefined) {
+        return
+      }
+
       editor.edit((builder: vscode.TextEditorEdit) => {
-        builder.replace(range, res);
-      });
+        builder.replace(range, res)
+      })
     } catch (e) {
-      handleTidyError(e);
-      return;
+      handleTidyError(e)
+      return
     }
-  });
+  })
 
   promiseVersion.then((version: Number) => {
     perltidyVersion = version
 
-    context.subscriptions.push(provider);
-    context.subscriptions.push(formatOnTypeProvider);
-    context.subscriptions.push(command);
+    context.subscriptions.push(provider)
+    context.subscriptions.push(formatOnTypeProvider)
+    context.subscriptions.push(command)
   }).catch(error => {
     vscode.window.showErrorMessage(error)
   })
